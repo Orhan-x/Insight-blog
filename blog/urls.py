f8892646cf3fe03c5d427e33d78ebf289b997f08
Orhan-x <oumoussacontact@gmail.com>
2024-01-24 16:43:44 +0100

    add all needed Models

--- blog/urls.py
+++ blog/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("bookstore")),
+    path('', include("bookstore.urls")),
 ]
--- bookstore/models.py
+++ bookstore/models.py
@@ -2,16 +2,31 @@ from django.db import models
 
 # Create your models here.
 class Author(models.Model):
-    name = models.CharField(max_length = 30)
-    email = models.EmailField()
-    passwd = models.CharField(min_length = 10)
-    is_active = models.BooleanField(default=False)
     
+    name = models.CharField(max_length = 30)
+    email = models.EmailField(unique = True)
+    bio = models.TextField()
+    
+    def __str__(self):
+        return self.name
+    
+class Category(models.Model):
+    
+    Categories  = (
+        ('Cyber security','Cyber security'),
+        ('Front-end','Front-end'),
+        ('Back-end','Back-end'),
+        ('Big Data','Big Data'),
+        ('AI','AI'),
+        ('Data Engineer','Data Engineer'),
+        ('Data Analysis','Data Analysis'),
+    )
+    name = models.CharField(max_length = 40,null=True, choices = Categories)
     
     
 class Post(models.Model):
-    image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=None)
-    author = models.ForeignKey()
-    
-class Order(models.Model):
+    pass
+
+
+class Comment(models.Model):
     pass
