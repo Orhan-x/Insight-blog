782bb6c8435fc4771cffeacfb5d5a81944afe135
Orhan-x <oumoussacontact@gmail.com>
2024-01-31 17:45:02 +0100

    Feat: Change the project architecture

--- bookstore/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Category)
-admin.site.register(User)
-admin.site.register(Post)
-admin.site.register(Author)
-admin.site.register(Comment)
-admin.site.register(Tag)
