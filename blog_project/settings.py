8e8d2a93a2fd02c18ecf0afed857addd0efbfc0f
Orhan-x <oumoussacontact@gmail.com>
2024-02-01 15:21:12 +0100

    add create post view to urlpatterns

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -52,11 +52,13 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'blog_project.urls'
-
+import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
