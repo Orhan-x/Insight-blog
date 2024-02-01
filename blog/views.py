45d5dce963900b74076ef78a368fe221387af352
Orhan-x <oumoussacontact@gmail.com>
2024-02-01 15:20:11 +0100

    add Create Post View

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .forms import PostForm
 
-# Create your views here.
+def home(request):
+    
+    context = {}
+    
+    return render(request, 'blog/home.html', context)
+
+
+def create_post(request):
+    context = {}
+    
+    if request.method == "GET":
+        form = PostForm()
+        context['form'] = form
+        return render(request, 'blog/create_post.html', context)
+    if request.method == 'POST':
+        pass
+    
