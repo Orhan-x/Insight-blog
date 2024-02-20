65a11b3410c715ff0283f75ab5f59d09777b5e22
Orhan-x <oumoussacontact@gmail.com>
2024-02-20 20:45:28 +0100

    add: registration ModelForm

---
+++ users/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm
+from django.contrib.auth.models import User
+
+class RegistrationForm(ModelForm):
+        class Meta:
+                model = User
+                fields = [
+                        "first_name",
+                        "last_name",
+                        "email",
+                        "password",
+                        "date_joined"
+                ]
