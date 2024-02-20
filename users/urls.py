22ebdd09fd68f22efa5a73395edcd4775df012de
Orhan-x <oumoussacontact@gmail.com>
2024-02-20 20:33:48 +0100

    better path

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("user/register",views.registration, name="register"),
+    path("register",views.registration, name="register"),
 ]
