9c2925d9133df60e9f22e23cf9aaf8eadda3de9b
Orhan-x <oumoussacontact@gmail.com>
2024-02-23 00:26:40 +0100

    add: Post Model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from uuid import uuid4
+from users.models import User
         
-            
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default = uuid4)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    posted_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
