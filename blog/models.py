cc83994ac2a67a476b8df411717fcd4a78da4b0b
Orhan-x <oumoussacontact@gmail.com>
2024-02-22 21:55:12 +0100

    Refactoring code

--- blog/models.py
+++ blog/models.py
@@ -1,23 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import User
-
-class Post(models.Model):
-    
-    title = models.CharField(max_length=120)
-    content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    publish_at = models.DateTimeField(auto_now=True)
-    is_updated = models.BooleanField(default=False)
-    
-    def save(self, *args, **kwargs):
-        
-        if self.pk : 
-            try:
-                old_instance = Post.objects.get(pk = self.pk)
-                if old_instance.title != self.title or old_instance.content != self.content:
-                    self.is_updated = True
-            except Post.DoesNotExist:
-                pass
-        super().save(self, *args, **kwargs)
         
             
--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,1 @@
 from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('', views.home, name="posts"),
-    path('home/', views.home, name="posts"),
-    path('post/create/', views.create_post, name="create-post"),
-]
