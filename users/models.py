5274e699712a7223968c080fec8ec79b8e0015e1
Orhan-x <oumoussacontact@gmail.com>
2024-02-23 00:06:24 +0100

    make sure username is lower

--- users/models.py
+++ users/models.py
@@ -6,3 +6,7 @@ class User(AbstractUser):
     avatar = models.TextField(default='/media/users/default-avatar.png')
     about = models.TextField(null=True, blank=True, max_length="300")
     email = models.EmailField(null=False, unique=True)
+
+    def save(self, *args, **kwargs):
+        self.username = self.username.lower()
+        super().save(*args, **kwargs)
