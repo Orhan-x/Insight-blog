0090918f167acc9967e498a3d464a0e138d5da38
Orhan-x <oumoussacontact@gmail.com>
2024-02-25 23:24:53 +0100

    Small changes

--- users/views.py
+++ users/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render,redirect
-from .forms import RegistrationForm
+from .models import User
 
 def registration(request):
     context = {}
-    if request.method == "GET":
-        return render(request, 'register.html', context)
-    if request.method == "POST":
-        
 
-        return redirect('posts', 1)
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        User.objects.create( username = username,  email = email, password = password)
+        return redirect('posts')
+    
+    return render(request, 'register.html', context)
