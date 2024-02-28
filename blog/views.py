1ebba57f7b2b902acf16d509ded855210887a98a
Orhan-x <oumoussacontact@gmail.com>
2024-02-28 01:48:54 +0100

    Valid Form and it's registration on DB

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render 
+from django.shortcuts import render , redirect
 from .forms import PostForm
 
 
@@ -6,8 +6,10 @@ from .forms import PostForm
 def create_post(request):
     context  = {}
     if request.method == "POST":
-        # logic stuff
-        pass
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('posts')
     context['form'] = PostForm()
     return render(request, 'blog/create_post.html', context)
 
