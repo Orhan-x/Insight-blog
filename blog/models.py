b3caf461e364aa81a171d0ac224be8734b1b04eb
Orhan-x <oumoussacontact@gmail.com>
2024-02-28 01:59:51 +0100

    Default indication of post on DB

--- blog/models.py
+++ blog/models.py
@@ -9,3 +9,5 @@ class Post(models.Model):
     content = models.TextField()
     posted_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.title
